73aba37381e9003fe7acf5f4cac0c64775a53c9f
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-01-30 09:51:40 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 /** @format */
 
+let header = document.getElementsByTagName("header")[0];
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 360) {
+    header.classList.add("headerJS");
+  } else {
+    header.classList.remove("headerJS");
+  }
+});
+/************************************************ */
 let menu = document.querySelector(".menu");
 let closeMenu = document.querySelector(".closeMenu");
 let openMenu = document.querySelector(".openMenu");
@@ -11,11 +21,16 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   menu.classList.remove("menuInjs");
 });
+/*******************************LES LIENS DE NAVIGATION********************************* */
 
-/******************************************************* */
+let link = document.querySelectorAll(".link");
+for (let x = 0; x < link.length; x++) {
+  link[x].addEventListener("click", () => {
+    menu.classList.remove("menuInjs");
+  });
+}
+/************************************************************* */
 let bloc_service = document.querySelectorAll(".bloc_service");
-let texteCommande = document.createElement("h4");
-texteCommande.innerHTML = "allez sur contact pour commander celui-ci";
 for (let j = 0; j < bloc_service.length; j++) {
   bloc_service[j].addEventListener("mouseover", () => {
     bloc_service[j].classList.add("bloc_service_Js");
@@ -23,12 +38,10 @@ for (let j = 0; j < bloc_service.length; j++) {
       "title",
       "Nous sommes capable de vous le concevoir"
     );
-    bloc_service[j].appendChild(texteCommande);
   });
 
   bloc_service[j].addEventListener("mouseout", () => {
     bloc_service[j].classList.remove("bloc_service_Js");
-    bloc_service[j].removeChild(texteCommande);
   });
 }
 /********************************************** */
