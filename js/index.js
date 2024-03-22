ec7eb4665a5274de1a4ce68310b4e13f9c14539a
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-03-22 20:57:10 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -20,21 +20,11 @@ for (let x = 0; x < link.length; x++) {
     menu.classList.remove("menuInjs");
   });
 }
-/************************************************************* */
-let bloc_service = document.querySelectorAll(".bloc_service");
-for (let j = 0; j < bloc_service.length; j++) {
-  bloc_service[j].addEventListener("mouseover", () => {
-    bloc_service[j].classList.add("bloc_service_Js");
-    bloc_service[j].setAttribute(
-      "title",
-      "Nous sommes capable de vous le concevoir"
-    );
-  });
-
-  bloc_service[j].addEventListener("mouseout", () => {
-    bloc_service[j].classList.remove("bloc_service_Js");
-  });
-}
+/******************************************** */
+let body=document.getElementsByTagName("body")[0]
+body.addEventListener("sroll",()=>{
+  alert("édsuifhmnkfg")
+})
 /********************************************** */
 let membreEquipe = document.querySelectorAll(".bloc_membre_equipe");
 for (let i = 0; i < membreEquipe.length; i++) {
