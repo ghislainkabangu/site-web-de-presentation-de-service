a0320decda89be5fa934496ed1da645b621253f3
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-04-04 09:55:31 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -20,32 +20,12 @@ for (let x = 0; x < link.length; x++) {
     menu.classList.remove("menuInjs");
   });
 }
-/******************************************** */
-let body=document.getElementsByTagName("body")[0]
-body.addEventListener("sroll",()=>{
-  alert("édsuifhmnkfg")
-})
-/********************************************** */
-let membreEquipe = document.querySelectorAll(".bloc_membre_equipe");
-for (let i = 0; i < membreEquipe.length; i++) {
-  membreEquipe[i].addEventListener("mouseover", () => {
-    membreEquipe[i].classList.add("bloc_membre_equipe_js");
-  });
-
-  membreEquipe[i].addEventListener("mouseout", () => {
-    membreEquipe[i].classList.remove("bloc_membre_equipe_js");
-  });
-}
-/********************************************* */
-let contact = document.querySelectorAll(".contact");
-for (let y = 0; y < contact.length; y++) {
-  contact[y].addEventListener("mouseover", () => {
-    contact[y].innerHTML = "consulter";
-  });
-
-  contact[y].addEventListener("mouseout", () => {
-    contact[y].innerHTML = "contacter";
-  });
-  contact[y].title = "Cliquez ici pour le consulter";
-}
-/*********************************************** */
+/**********************gestion des interactions images********************** */
+let image = document.getElementById("image");
+image.addEventListener("mouseover", () => {
+  image.src = "album/fond2.png";
+});
+image.addEventListener("mouseout", () => {
+  image.src = "album/logo.png";
+});
+/*********************************** */
